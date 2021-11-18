e9d80c6f8763b0d3de5b28266a4392b732f46665
Татьяна Анисимова <kulmina@mail.ru>
2021-11-18 18:51:12 +0300

    Добавьте файлы проекта.

---
+++ Lab4_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+            int N;
+            int sum;
+            Console.WriteLine("Введите число");
+            N = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <=N; i++)
+            {
+                Console.WriteLine("Квадрат числа {0} равен {1}", i, i * i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
